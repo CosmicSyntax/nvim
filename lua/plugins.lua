21b61eb334a36ee320970e34eda46560b31d5ee4
Daniel Choi <choi.mina@gmail.com>
2021-11-12 00:58:16 -0500

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,6 @@
 return require('packer').startup(function()
 use 'wbthomason/packer.nvim'
-use 'nvim-lualine/lualine.nvim'
-use 'kyazdani42/nvim-web-devicons'
+use 'nvim-lualine/lualine.nvim' 
 use 'akinsho/bufferline.nvim'
 use { 'mg979/vim-visual-multi', branch = 'master' }
 use 'preservim/nerdcommenter'
@@ -10,7 +9,7 @@ use 'junegunn/fzf.vim'
 use 'airblade/vim-gitgutter'
 use 'tpope/vim-rhubarb'
 use 'tpope/vim-fugitive'
-use 'sindrets/diffview.nvim'
+use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 use 'akinsho/toggleterm.nvim'
 use 'sebdah/vim-delve'
 use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
@@ -22,7 +21,7 @@ use 'hrsh7th/nvim-cmp'
 use 'hrsh7th/cmp-nvim-lsp'
 use 'L3MON4D3/LuaSnip'
 use 'saadparwaiz1/cmp_luasnip'
-use 'kyazdani42/nvim-tree.lua'
+use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
 use 'folke/trouble.nvim'
 use 'EdenEast/nightfox.nvim'
 use 'karb94/neoscroll.nvim'
