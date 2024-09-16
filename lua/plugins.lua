c4c55c1b58b9ee953b71026cbcf333925ad193df
Cosmic Syntax <choi.mina@gmail.com>
2024-09-16 00:01:19 -0400

    adding noice

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -108,6 +108,33 @@ require("lazy").setup({
 				require('lazyConfigs/flash')
 			end,
 		},
+		-- lazy.nvim
+		{
+			"folke/noice.nvim",
+			event = "VeryLazy",
+			opts = {
+				views = {
+					cmdline_popup = {
+						position = {
+							row = "50%",
+							col = "50%",
+						},
+						size = {
+							width = 75,
+							height = "auto",
+						},
+					},
+				},
+			},
+			dependencies = {
+				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+				"MunifTanjim/nui.nvim",
+				-- OPTIONAL:
+				--   `nvim-notify` is only needed, if you want to use the notification view.
+				--   If not available, we use `mini` as the fallback
+				-- "rcarriga/nvim-notify",
+			}
+		},
 		'github/copilot.vim',
 		{
 			'sebdah/vim-delve',
