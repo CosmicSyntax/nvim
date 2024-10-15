c386d9e89360f103ff8fa33785d1d5f1be03836b
Cosmic Syntax <choi.mina@gmail.com>
2024-10-15 16:56:41 -0400

    replacing node with deno 2.0

--- lua/lazyConfigs/ts.lua
+++ lua/lazyConfigs/ts.lua
@@ -3,20 +3,23 @@ local cmp = require('cmp_nvim_lsp')
 local capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Typescript
-nvim_lsp.tsserver.setup({
+-- nvim_lsp.tsserver.setup({
+-- 	capabilities = capabilities,
+-- 	init_options = {
+-- 		preferences = {
+-- 			includeInlayParameterNameHints = 'all',
+-- 			includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+-- 			includeInlayFunctionParameterTypeHints = true,
+-- 			includeInlayVariableTypeHints = true,
+-- 			includeInlayPropertyDeclarationTypeHints = true,
+-- 			includeInlayFunctionLikeReturnTypeHints = true,
+-- 			includeInlayEnumMemberValueHints = true,
+-- 			importModuleSpecifierPreference = 'non-relative',
+-- 		},
+-- 	},
+-- })
+nvim_lsp.denols.setup({
 	capabilities = capabilities,
-	init_options = {
-		preferences = {
-			includeInlayParameterNameHints = 'all',
-			includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-			includeInlayFunctionParameterTypeHints = true,
-			includeInlayVariableTypeHints = true,
-			includeInlayPropertyDeclarationTypeHints = true,
-			includeInlayFunctionLikeReturnTypeHints = true,
-			includeInlayEnumMemberValueHints = true,
-			importModuleSpecifierPreference = 'non-relative',
-		},
-	},
 })
 
 nvim_lsp.html.setup({
