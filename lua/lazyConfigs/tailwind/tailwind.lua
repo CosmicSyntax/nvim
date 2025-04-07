42eb598b8025669a786803ce9578fb387554e9bd
Cosmic Syntax <choi.mina@gmail.com>
2025-04-07 12:39:38 -0400

    tailwindcss lsp fix

--- lua/lazyConfigs/tailwind/tailwind.lua
+++ lua/lazyConfigs/tailwind/tailwind.lua
@@ -1,9 +1,62 @@
 local capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Enable TailwindCSS-LSP
-vim.lsp.config["tailwindcss"] = {
+vim.lsp.config["tw"] = {
 	capabilities = capabilities,
-	cmd = { "tailwindcss-language-server", "--stdio" },
-	filetypes = { "html", "htmldjango", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte", "vue" },
+	cmd = { 'tailwindcss-language-server', '--stdio' },
+	-- filetypes copied and adjusted from tailwindcss-intellisense
+	filetypes = {
+		-- html
+		'django-html',
+		'htmldjango',
+		'gohtml',
+		'gohtmltmpl',
+		'html',
+		'htmlangular',
+		'markdown',
+		-- css
+		'css',
+		'less',
+		'postcss',
+		'sass',
+		'scss',
+		'stylus',
+		'sugarss',
+		-- js
+		'javascript',
+		'javascriptreact',
+		'typescript',
+		'typescriptreact',
+		-- mixed
+		'vue',
+		'svelte',
+		'templ',
+	},
+	settings = {
+		tailwindCSS = {
+			validate = true,
+			lint = {
+				cssConflict = 'warning',
+				invalidApply = 'error',
+				invalidScreen = 'error',
+				invalidVariant = 'error',
+				invalidConfigPath = 'error',
+				invalidTailwindDirective = 'error',
+				recommendedVariantOrder = 'warning',
+			},
+			classAttributes = {
+				'class',
+				'className',
+				'class:list',
+				'classList',
+				'ngClass',
+			},
+		},
+	},
+	root_markers = {
+		'package.json',
+		'tailwind.config.js',
+		'tailwind.config.ts',
+	},
 }
-vim.lsp.enable('tailwindcss')
+vim.lsp.enable('tw')
