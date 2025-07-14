33e42e0046516709aaffd6f152d41973fe4974b2
Cosmic Syntax <choi.mina@gmail.com>
2025-07-13 23:43:00 -0400

    flash update

--- lua/lazyConfigs/flash.lua
+++ lua/lazyConfigs/flash.lua
@@ -3,3 +3,5 @@ local map = vim.api.nvim_set_keymap
 
 map('n', 's', '<cmd>lua require("flash").jump()<CR>', { noremap = true, silent = true })
 map('n', 'S', '<cmd>lua require("flash").treesitter()<CR>', { noremap = true, silent = true })
+
+require("flash").setup()
