fde75769722bee5d653991ba166d0be08a062434
Cosmic Syntax <choi.mina@gmail.com>
2026-05-07 02:19:04 -0400

    treesitter fix for 0.12

--- lua/lazyConfigs/shared.lua
+++ lua/lazyConfigs/shared.lua
@@ -1,6 +1,9 @@
 -- lua/lazyConfigs/shared.lua
 local M = {}
 
+-- Start Treesitter to ensure it's active for all filetypes
+vim.treesitter.start()
+
 -- Fetch the default capabilities and merge them with blink.cmp
 M.capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
